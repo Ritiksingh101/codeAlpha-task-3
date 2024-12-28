bf599c1bf6a4830de32670a62f36bd5e5661f323
Ritiksingh101 <rs1982thakur@gmail.com>
2024-12-28 12:17:25 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,11 @@
+function show(val){
+    document.getElementById("a").value +=val;
+}
+let sum=()=>{
+    let x=document.getElementById("a").value;
+    let=y=eval(x)
+    document.getElementById("a").value=y;
+}
+const clr=()=>{
+    document.getElementById("a").value =" ";
+}
